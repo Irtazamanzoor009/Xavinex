59152cf080169e6b8e5e58d141e69dc2844e92f6
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2025-07-09 17:10:38 +0500

    change minor spacing

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({ title, products }) => {
     <div className="p-4 rounded-lg shadow bg-white flex-1 min-w-[250px]">
       <h2 className="text-lg font-semibold mb-4">{title}</h2>
       {products.map((item, idx) => (
-        <div key={idx} className="mb-2">
+        <div key={idx} className="mb-4">
           <div className="flex justify-between text-sm">
             <span>{item.name}</span>
             <span>{item.sold} Sold</span>
